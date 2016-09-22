8ae13844d13f0f99310e849fe4353bab20f7b595
duoduo369 <duoduo3369@gmail.com>
2016-09-22 16:13:59 +0800

    add LICENSE setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='retry',
+    version='0.1',
+    url='http://www.djangoproject.com/',
+    author='duoduo3369',
+    author_email='duoduo3369@gmail.com',
+    description=('retry your function call.'),
+    keywords='retry',
+    license='MIT',
+    test_suite='test_retry',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: MIT'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Utilities',
+    ],
+)
