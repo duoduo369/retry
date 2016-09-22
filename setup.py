bbf4f5abd9448a160fede19e685cc22d7155ffd6
duoduo369 <duoduo3369@gmail.com>
2016-09-22 16:17:43 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='retry',
+    name='retry-function',
     version='0.1',
-    url='http://www.djangoproject.com/',
+    url='https://github.com/duoduo369/retry',
     author='duoduo3369',
     author_email='duoduo3369@gmail.com',
     description=('retry your function call.'),
-    keywords='retry',
+    keywords='retry retry-function retrying',
     license='MIT',
     test_suite='test_retry',
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: MIT'
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet',
